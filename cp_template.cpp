6e646808cc21e4f9f5cbaa1799a0eefbb3f3823a
aboubakr-jelloulat <abubakerjelloulat@gmail.com>
2025-05-19 16:11:47 +0100

    feat (template) : Add

--- cp_template.cpp
+++ cp_template.cpp
@@ -47,6 +47,36 @@ typedef vector<ll> vll;
 typedef vector<vector<ll> > vvll;
 typedef vector<vector<int> > vvi; 
 
+
+using namespace std;
+typedef long long ll;
+typedef long double lld;
+typedef map<string, int> map1;
+using namespace std;
+// #define endl '\n';
+ 
+ll gcd(ll a, ll b) { return b ? gcd(b, a % b) : a; }
+ 
+#define fast               \
+  ios::sync_with_stdio(0); \
+  cin.tie(0);              \
+  cout.tie(0);
+#define all(v) ((v).begin()), ((v).end())
+#define pb push_back
+#define MP make_pair
+#define py cout << "YES\n";
+#define pn cout << "NO\n"
+#define clr(x, val) memset(x, val, sizeof(x))
+#define OUT 0
+#define IN 1
+#define getin(n) for (int i = 0; i < n; ++i)
+#define l(i, s, n) for (int i = s; i < n; i++)
+#define le(i, s, n) for (int i = s; i <= n; i++)
+#define ln(i, e, n) for (ll i = n; i >= e; i--)
+
+
+typedef  vector <string> vs;
+
 // Loops and iteration
 #define all(v) v.begin(), v.end()
 #define rall(v) v.rbegin(), v.rend()
